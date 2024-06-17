fb83b2473adfd6aa20484c9e8a3313792db835c2
kamalika0363 <kamalikaghora14@gmail.com>
2024-06-17 18:46:46 +0530

    sh file

--- start_app_flask.sh
+++ start_app_flask.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-cd /home/omcloudops/flask-ml
-
-if [ ! -d "myenv" ]; then
-        python3.10 -m venv myenv
-fi
-
-source myenv/bin/activate
-
 pip3 install -r requirements.txt
 
 python3.10 app.py --host=0.0.0.0
